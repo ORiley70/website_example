2eda0a0d6eb9ed6403c38b1f24eab022da7f9819
ORiley70 <93169935+ORiley70@users.noreply.github.com>
2021-11-23 16:09:18 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+var button = document.getElementById("apply-button");
+
+button.onmouseover = function () {
+  this.style.backgroundColor = "#8b0000";
+};
+
+button.onmouseover = function () {
+  this.style.backgroundColor = "#BB2026";
+};
